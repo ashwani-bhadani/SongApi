5f9b060f72c53eddd972915128165c270a6183da
ashwani <ashwani.bhadani.3289@gmail.com>
2024-09-13 02:08:16 +0530

    FEAT: created basic ai template

---
+++ src/test/java/com/poc/ai/SongsApi/SongsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.ai.SongsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
